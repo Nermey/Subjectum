84bc580d5c1890f027330906a4ee0aaa90d07cea
Nermey <elik19898@gmail.com>
2023-12-03 00:45:33 +0300

    add orm database model and async service

--- src/auth.py
+++ src/auth.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from sqlalchemy import select, and_
+from orm import Authorization
 
-from database import session_local, engine
-from models import Users, Base
 
 app = FastAPI()
-# Base.metadata.drop_all(engine)
-Base.metadata.create_all(engine)
+Authorization.create_tables()
 
 
 class User(BaseModel):
@@ -22,36 +19,15 @@ class User_login(BaseModel):
 
 
 @app.post("/register")
-def register(user: User):
-    with session_local() as session:
-        user_obj = Users(email=user.email, password=user.password, name=user.name)
-        if check_user_exist(user.email):
-            session.add(user_obj)
-            session.commit()
-            raise HTTPException(status_code=200, detail="user is added successfully")
-    raise HTTPException(status_code=201, detail="user is already exist")
+async def register(user: User):
+    if await Authorization.check_user_exist(user.email):
+        raise HTTPException(status_code=201, detail="user is already exist")
+    await Authorization.add_new_user(user.email, user.password, user.name)
+    raise HTTPException(status_code=200, detail="user is successfully added")
 
 
 @app.post("/login")
-def login(user: User_login):
-    if check_user_exist(user.email):
-        raise HTTPException(status_code=202, detail="user is not exist")
-    if get_user_authorization(user.email, user.password):
+async def login(user: User_login):
+    if await Authorization.get_user_authorization(user.email, user.password):
         raise HTTPException(status_code=203, detail="authorization is successful")
-    raise HTTPException(status_code=400, detail="login or password are incorrect")
-
-
-def check_user_exist(email):
-    with session_local() as session:
-        query = select(Users).filter_by(email=email)
-        res = session.execute(query)
-        user = res.first()
-        return user is None
-
-
-def get_user_authorization(email, password):
-    with session_local() as session:
-        query = select(Users).filter_by(email=email, password=password)
-        res = session.execute(query)
-        user = res.first()
-        return user is not None
+    raise HTTPException(status_code=203, detail="login or password are incorrect")
--- src/orm.py
+++ src/orm.py
@@ -1,33 +1,33 @@
-from sqlalchemy import select, and_
-from database import engine, session_local
-from models import meta_data_obj, Users
+from database import Base, engine, session_local
+from models import Users
+from sqlalchemy import select
 
 
-def create_tables(): # где создавать таблицы?
-    meta_data_obj.drop_all(engine)
-    meta_data_obj.create_all(engine)
+class Authorization:
+    @staticmethod
+    async def create_tables():
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
 
+    @staticmethod
+    async def check_user_exist(email):
+        async with session_local() as session:
+            query = select(Users).filter_by(email=email)
+            res = await session.execute(query)
+            user = res.first()
+            return user is not None
 
-def add_new_user(login, password, name): # передавать pydantic model
-    new_user = Users(login=login, password=password, name=name)
-    with session_local() as session:
-        session.add(new_user)
-        session.commit()
+    @staticmethod
+    async def get_user_authorization(email, password):
+        async with session_local() as session:
+            query = select(Users).filter_by(email=email, password=password)
+            res = await session.execute(query)
+            user = res.first()
+            return user is not None
 
-
-def check_user_exist(login):
-    with session_local() as session:
-        query = select(Users).filter_by(login=login)
-        res = session.execute(query)
-        user = res.first()
-        return user is None  # лютый костыль
-
-
-def get_user_authorization(login, password):
-    with session_local() as session:
-        query = select(Users).filter(and_(Users.login == login,
-                                             Users.password == password
-                                             ))
-        res = session.execute(query)
-        user = res.first()
-        return user is None  # такая же история
+    @staticmethod
+    async def add_new_user(email, password, name):  # нужно ли импортировать класс User из auth (pydantic)
+        async with session_local() as session:
+            user_obj = Users(email=email, password=password, name=name)
+            session.add(user_obj)
+            await session.commit()
